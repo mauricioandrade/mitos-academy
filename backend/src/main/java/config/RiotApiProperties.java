0e6d13fbcc07482a39441f0b152b1a94a709e45d
mauricioandrade <mauricio_andrade@outlook.com>
2025-12-08 01:07:16 -0300

    config: ajusta propriedades da Riot API

--- backend/src/main/java/config/RiotApiProperties.java
+++ backend/src/main/java/config/RiotApiProperties.java
@@ -1,4 +1,30 @@
 package config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "riot")
 public class RiotApiProperties {
+
+    private String apiKey;
+
+    private String region;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
+
