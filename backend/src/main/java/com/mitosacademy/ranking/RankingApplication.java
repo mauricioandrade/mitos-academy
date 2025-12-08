057111fc5dd0be4df30ee753e04b232a9fe78475
mauricioandrade <mauricio_andrade@outlook.com>
2025-12-08 01:07:19 -0300

    chore: ajusta inicialização da aplicação

--- backend/src/main/java/com/mitosacademy/ranking/RankingApplication.java
+++ backend/src/main/java/com/mitosacademy/ranking/RankingApplication.java
@@ -1,9 +1,14 @@
 package com.mitosacademy.ranking;
 
+import config.RiotApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RiotApiProperties.class)
+@EnableScheduling
 public class RankingApplication {
 
 	public static void main(String[] args) {
